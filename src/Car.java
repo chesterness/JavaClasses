ff6ff12dde6dcceecd4579f5338c0cd6070c1e63
chesterness <martaaania@gmail.com>
2020-11-04 08:33:43 +0100

    created Car class and Car object

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public String manufacturer;
+    public String colour;
+    private String vin;
+
+    public Car(String manufacturer, String colour){
+        this.manufacturer = manufacturer;
+        this.colour = colour;
+        this.vin = "ABC123";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car bmw = new Car("BMW", "blue");
+        System.out.println(bmw.manufacturer + " " + bmw.colour);
+    }
+}
